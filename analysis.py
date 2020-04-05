60e8b9c068361b140e39b4e10af4a8a355c39cdf
Killian Foley <killianfoley@Killian-Foleys-MacBook.local>
2020-04-05 18:47:39 +0100

    initial commit

---
+++ analysis.py
@@ -0,0 +1,5 @@
+#This program shall perform the following actions
+#Output a summary of each variable to a single text file
+#Save histogram of each variable to png fils
+#Outputs a scatter plot of each pair of variables
+
